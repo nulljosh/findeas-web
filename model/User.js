c6dd9dfa686418f9c3b8618c9dc6e3aedb6dcde2
Joshua Trommel <trommatic@icloud.com>
2025-03-19 11:19:13 -0700

    X

---
+++ model/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+    , bcrypt = require('bcrypt');
+
+const SALT = 10;
+
+const userSchema = mongoose.Schema({
+    username: {type: String, required: true},
+    password: {type: String, required: true}
+});
+
+userSchema.pre('save', function(next) {
+    var user = this;
+
+    if (!user.isModified('password')) return next();
+    bcrypt.genSalt(SALT, function(err, salt) {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            //override cleartext pw with hash
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+module.exports = mongoose.model('User', userSchema);
